13ef5cd378beea9e3b696316c1e4a817ec8fa6b6
Leon Li <qianthinking@gmail.com>
2014-01-09 11:21:05 +0800

    air sdk default 3.8

--- tools/compile.sh
+++ tools/compile.sh
@@ -8,7 +8,7 @@ project_name=$(basename $(pwd))
 
 mxmlc \
 -library-path /usr/local/Cellar/flex_sdk/4.6.0.23201/libexec/frameworks/libs \
--library-path /usr/local/Cellar/adobe-air-sdk/3.5/libexec/frameworks/libs/air \
+-library-path /usr/local/Cellar/adobe-air-sdk/3.8/libexec/frameworks/libs/air \
 -show-actionscript-warnings -show-binding-warnings \
 -static-link-runtime-shared-libraries \
 -output bin/$project_name.swf -- src/$project_name.as
